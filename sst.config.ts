1a617513b52f854761c68ccd8cb9b261d8e80292
Amo Moloko <amomoloko@naked.insure>
2025-04-18 20:17:47 +0200

    updates

--- sst.config.ts
+++ sst.config.ts
@@ -4,6 +4,11 @@ export default $config({
   app(input) {
     return {
       name: 'real-world-agents-mcp',
+      providers: {
+        aws: {
+          profile: 'agents-mcp',
+        },
+      },
       removal: input?.stage === 'production' ? 'retain' : 'remove',
       protect: ['production'].includes(input?.stage),
       home: 'aws',
